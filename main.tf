c8d5d58073b52d74a118cfefc2539d79cf4f43a7
Durga217 <185882749+Durga217@users.noreply.github.com>
2025-01-28 14:29:37 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "var.aws_region"
 }
 resource "aws_instance" "example" {
   ami           = "ami-00bb6a80f01f03502"
-  instance_type = "t2.micro"
-  key_name      = "Durga-Test"
+  instance_type = "var.instance_type"
+  key_name      = "var.key_name"
   subnet_id     = "subnet-0e67a89dc05c9fad9"
   vpc_security_group_ids = ["sg-04a64f749116231c8"]
   associate_public_ip_address = true
