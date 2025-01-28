b71b6b8a23873360477162a8edf6758e9c35da31
Durga217 <185882749+Durga217@users.noreply.github.com>
2025-01-28 14:17:07 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,17 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow HTTP and SSH access"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_instance" "web_server" {
-  ami           = "ami-053b12d3152c0cc71" # Replace with the appropriate AMI
+resource "aws_instance" "example" {
+  ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
-  key_name      = "Your-Key-Pair-Name"    # Replace with your key pair name
-  security_groups = [aws_security_group.web_sg.name]
+  key_name      = "Durga-Test"
+  subnet_id     = "subnet-0e67a89dc05c9fad9"
+  vpc_security_group_ids = ["sg-04a64f749116231c8"]
+  associate_public_ip_address = true
   tags = {
-    Name = "Terraform-Instance"
+    Name = "MyTerraformInstance"
   }
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y docker.io
-              systemctl start docker
-              systemctl enable docker
-              mkdir /home/ubuntu/my-website
-              cd /home/ubuntu/my-website
-              echo 'FROM nginx:latest' > Dockerfile
-              echo 'COPY ./index.html /usr/share/nginx/html/index.html' >> Dockerfile
-              echo 'This page is hosted on an EC2 instance with Docker' > index.html
-              docker build -t my-web-app .
-              docker run -d -p 80:80 my-web-app
-            EOF
 }
-output "public_ip" {
-  value = aws_instance.web_server.public_ip
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
 }
